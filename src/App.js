f6263efd06146ca5de3cee0101381f883f006228
Tatiana Eskina <horechek444@gmail.com>
2021-03-16 21:57:43 +0300

    feat: настройка движений персонажа

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 
 const App = () => {
   const canvasRef = React.useRef(null);
+  const linkDownRef = React.useRef(null);
+  const linkRightRef = React.useRef(null);
+  const linkUpRef = React.useRef(null);
+  const linkLeftRef = React.useRef(null);
   const [x, setX] = React.useState(0);
   const [y, setY] = React.useState(0);
+  const [direction, setDirection] = React.useState('down');
 
-  const move = (direction) => {
-    if (direction === 'up') setY((y) => y - 20);
-    if (direction === 'left') setX((x) => x - 20);
-    if (direction === 'down') setY((y) => y + 20);
-    if (direction === 'right') setX((x) => x + 20);
+  const move = (dir) => {
+    setDirection(dir);
+    if (dir === 'up') setY((y) => y - 20);
+    if (dir === 'left') setX((x) => x - 20);
+    if (dir === 'down') setY((y) => y + 20);
+    if (dir === 'right') setX((x) => x + 20);
   };
 
   // без ререндера окно канваса
@@ -24,7 +30,14 @@ const App = () => {
   React.useEffect(() => {
     const context = canvasRef.current.getContext('2d');
     context.clearRect(0, 0, window.innerHeight, window.innerWidth);
-    context.fillRect(x, y, 100, 100);
+    // context.fillRect(x, y, 100, 100);
+    let theLinkRef;
+    if (direction === 'down') theLinkRef = linkDownRef;
+    if (direction === 'up') theLinkRef = linkUpRef;
+    if (direction === 'left') theLinkRef = linkLeftRef;
+    if (direction === 'right') theLinkRef = linkRightRef;
+
+    context.drawImage(theLinkRef.current, x, y);
   }, [x, y]);
 
   // слушатель событий окна для клавиш
@@ -52,10 +65,22 @@ const App = () => {
       </div>
 
       <div className="images">
-        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down"/>
-        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right"/>
-        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up"/>
-        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left"/>
+        <img
+          ref={linkDownRef}
+          src="https://i.imgur.com/JYUB0m3.png"
+          alt="Down"/>
+        <img
+          ref={linkRightRef}
+          src="https://i.imgur.com/GEXD7bk.gif"
+          alt="Right"/>
+        <img
+          ref={linkUpRef}
+          src="https://i.imgur.com/XSA2Oom.gif"
+          alt="Up"/>
+        <img
+          ref={linkLeftRef}
+          src="https://i.imgur.com/4LGAZ8t.gif"
+          alt="Left"/>
       </div>
     </div>
   );
